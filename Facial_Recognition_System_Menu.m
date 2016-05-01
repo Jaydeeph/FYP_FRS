9c062b6b996dca9308129c36d7deafa98f590a7c
jaydeeph <jaydeeph.vaja@gmail.com>
2016-05-01 21:58:39 +0100

    First commit. 6th iteration.

---
+++ Facial_Recognition_System_Menu.m
@@ -0,0 +1,42 @@
+%% Display The Main Menu:
+disp('"Face Recognition": To start detecting and Recognising a face.');
+disp('"Add To Database" : To add a person to the databse of faces.');
+disp('"Quit"            : To quit the application');
+
+%% Get Users' Input:
+User_Answer = lower(input('Please choose from the following options:', 's'));
+
+%% Load The Correct MATLAB File Depending on User Input via Switch Cases.
+switch User_Answer
+    case 'face recognition'
+        clear;
+        clc;
+        Facial_Recognition_System;
+    case 'add to database'
+        clear;
+         clc;
+        Add_Person_To_Database;
+    case 'quit'
+        clear;
+        clc;
+        
+    otherwise
+        clear;
+        clc;
+        Facial_Recognition_System;
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+#FYP_FRS Face Detection and Recognition is my Final Year Project that is coded in MATLAB. As I'm using prototyping, iterative and incremental methodology; this is my 6th iteration/prototype.
