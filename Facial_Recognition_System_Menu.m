886020f4716d64ae37ca1f6603535cf1e9926362
jaydeeph <jaydeeph.vaja@gmail.com>
2016-05-02 03:35:51 +0100

    Modified the Facial_Recognition_System class. Made the code run faster than previous. Coded it, so the HOG features array lenght is dynamically created by analysing the image from the dataabse. Added voice features when a face is detected and lost. Added emailing feature that would email the user of an intruder.

--- Facial_Recognition_System_Menu.m
+++ Facial_Recognition_System_Menu.m
@@ -4,7 +4,7 @@ disp('"Add To Database" : To add a person to the databse of faces.');
 disp('"Quit"            : To quit the application');
 
 %% Get Users' Input:
-User_Answer = lower(input('Please choose from the following options:', 's'));
+User_Answer = lower(input('Please choose from the following options: ', 's'));
 
 %% Load The Correct MATLAB File Depending on User Input via Switch Cases.
 switch User_Answer
